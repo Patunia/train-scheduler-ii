65b33c4c2a0304b0193e98f460a86874abb5d50d
patunia <pborowiec@alumni.nd.edu>
2018-07-30 12:46:10 -0500

    firebase data

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyC7n7QG81iC1lppPnSLG1wonU5GeUpMQls",
+    authDomain: "train-schedule-9b40e.firebaseapp.com",
+    databaseURL: "https://train-schedule-9b40e.firebaseio.com",
+    projectId: "train-schedule-9b40e",
+    storageBucket: "",
+    messagingSenderId: "300417295759"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
