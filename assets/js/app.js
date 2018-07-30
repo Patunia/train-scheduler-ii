25d4798fcb13d420fe88f4461780185356ffbd36
patunia <pborowiec@alumni.nd.edu>
2018-07-30 16:15:19 -0500

    current time working

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,10 +20,29 @@ var config = {
 
   $("#train-entry").on("click", function() {
     event.preventDefault();
+});
 
-//firebase
+// train-example
+    var tFrequency = 3;
+    var firstTime = "00:00";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
     var currentTime = moment();
-    console.log("current time " + moment(currentTime).format("YYYY-MM-DD dddd HH:mm:ss));
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    $("#time").text(currentTime.format("YYYY-MM-DD dddd HH:mm:ss"));
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    $("#time").text(currentTime.format("YYYY-MM-DD dddd HH:mm"));
