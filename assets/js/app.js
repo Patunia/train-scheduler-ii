076d5e77c3d389e9a362dc73f86ca2240ba336be
patunia <pborowiec@alumni.nd.edu>
2018-07-30 16:51:13 -0500

    testing firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,50 +7,66 @@ var config = {
     projectId: "train-schedule-9b40e",
     storageBucket: "",
     messagingSenderId: "300417295759"
-  };
-  firebase.initializeApp(config);
+};
 
-  var database = firebase.database();
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 //moment
 
-  setInterval(function(startTime) {
+setInterval(function(startTime) {
     $("#time").html(moment().format('YYYY-MM-DD dddd HH:mm:ss'));
-  }, 1000);
+}, 1000);
 
-  $("#train-entry").on("click", function() {
+$("#train-entry").on("click", function() {
     event.preventDefault();
 });
 
 // firebase
 
-    var train = $("#input-train-name").val().trim();
-    var destination = $("#input-destination").val().trim();
-    var firstTime = $("#input-first-train-time").val().trim();
-    var frequency = $("#input-frequency").val().trim();
+var train = $("#input-train-name").val().trim();
+var destination = $("#input-destination").val().trim();
+var firstTime = $("#input-first-train-time").val().trim();
+var frequency = $("#input-frequency").val().trim();
 
+var trainData = {
+    formtrain: train,
+    formdestination: destination,
+    formfirsttime: firstTime,
+    formfrequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+
+database.ref().push(trainData);
+
+console.log(trainData.formtrain);
+console.log(trainData.formdestination);
+console.log(trainData.formfirsttime);
+console.log(trainData.formfrequency);
+console.log(trainData.dateAdded);
 
 // train-example in-class
-    var tFrequency = 3;
-    var firstTime = "00:00";
+var tFrequency = 3;
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+var firstTime = "00:00";
+
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
-    var currentTime = moment();
+var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    var tRemainder = diffTime % tFrequency;
+var tRemainder = diffTime % tFrequency;
     console.log(tRemainder);
 
-    var tMinutesTillTrain = tFrequency - tRemainder;
+var tMinutesTillTrain = tFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    $("#time").text(currentTime.format("YYYY-MM-DD dddd HH:mm"));
+$("#time").text(currentTime.format("YYYY-MM-DD dddd HH:mm"));
